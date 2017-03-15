00314c7e69a6e089e34794480e63041c39fd2d68
LitvinenkoIra <ilytvynenko654@gmail.com>
2017-03-15 20:09:11 +0200

    house

--- ComplexFunction/main.cpp
+++ ComplexFunction/main.cpp
@@ -14,166 +14,100 @@
 
 std::string WINDOW_NAME = "Win name";
 
-
-//using namespace std;
-//void generate(std::complex<double> result){
-//    static std::complex<double> previous_result = 0 + 0i;
-//    cout << "<line x1=\"" << previous_result.real() << "\" y1=\"" << previous_result.imag()
-//         << "\" x2=\"" << result.real() << "\" y2=\"" << result.imag() << "\" style=\"stroke:rgba(250,200,200,0.5);stroke-width:2\"/>" << std::endl;
-//    previous_result = result;
-//}
-
-class FuncR {
-public:
-    FuncR():
-        a_(1),
-        b_(1),
-        c_(1),
-        d_(1),
-        e_(1) {
-
-    }
-
-    void Draw() {
-        {
-            svg::SVGGraphMoved graph("test.svg", 600, 600);
-            double old_y = 0;
-            try {
-              old_y = func(0);
-            }catch (std::exception E){
-                std::cout << E.what() << std::endl;
-            }
-            double inc = 5;
-            for (double z = -200; z < 200; z += inc) {
-                try {
-                    double y = func(z);
-                    graph.line(z - inc, old_y, z , y);
-                    std::cout << " z = " << z << " y = " << y << std::endl;
-                    old_y = y;
-                } catch (std::exception E){
-                   std::cout << E.what() << std::endl;
-                }
-            }
-        }
-        system("convert test.svg test.jpg");
-        cv::Mat image = cv::imread("test.jpg");
-        cv::imshow( WINDOW_NAME, image );
-    }
-
-    double func(double z) {
-        if ((d_ * z + e_) == 0){
-            throw std::overflow_error("Divide by zero exception");
-        }
-
-        return (a_ * pow(z, 2) + b_ * z + c_) / (d_ * z + e_);
-    }
-
-    double a_;
-    double b_;
-    double c_;
-    double d_;
-    double e_;
-};
-
 std::complex<double> func(std::complex<double> b, std::complex<double> c, std::complex<double> e,
                           std::complex<double> a, std::complex<double> d, std::complex<double> z){
     return (a * pow(z,2) + b * z + c) / (d * z + e);
 }
 
+class House {
+public:
+    House (double weigth, double height,
+           double scale, double a):
+    weigth_(weigth),
+    height_(height),
+    scale_(scale),
+    a_(a ){}
 
+    //передавать граф в параметрах
+    void Draw() {
+        {
+            svg::PlotSVG graph("house.svg",(a_ * weigth_* weigth_)/scale_, height_/scale_);
+            graph.line(0, height_ * 0.7, weigth_/2, height_, svg::COLOR_GREEN);
+            graph.line(weigth_/2, height_, weigth_, height_*0.7, svg::COLOR_GREEN);
+            graph.line(weigth_, height_*0.7, weigth_, 0, svg::COLOR_GREEN);
+            graph.line(weigth_, 0, 0, 0, svg::COLOR_GREEN);
+            graph.line(0, 0, 0, height_*0.7, svg::COLOR_GREEN);
+            graph.line(weigth_*0.7, height_*0.6, weigth_*0.3, height_*0.6, svg::COLOR_GREEN);
+            graph.line(weigth_*0.3, height_*0.6, weigth_*0.3, height_*0.2, svg::COLOR_GREEN);
+            graph.line(weigth_*0.3, height_*0.2, weigth_*0.7, height_*0.2, svg::COLOR_GREEN);
+            graph.line(weigth_*0.7, height_*0.2, weigth_*0.7, height_*0.6, svg::COLOR_GREEN);
+            graph.point(0, 0, "red");
+            graph.point(weigth_*0.7, height_*0.6, "red");
+            graph.point(weigth_*0.3, height_*0.2, "red");
+            graph.point(weigth_*0.3, height_*0.6, "red");
+            graph.point(weigth_*0.7, height_*0.2, "red");
+            graph.point(0, height_ * 0.7, "red");
+            graph.point(weigth_, 0, "red");
+            graph.point(weigth_, height_* 0.7, "red");
+            graph.point(0,height_* 0.7, "red");
+            graph.point(weigth_ / 2, height_, "red");
+
+        }
+        system("convert house.svg house.jpg");
+        cv::Mat image = cv::imread("house.jpg");
+        cv::imshow( WINDOW_NAME, image );
+    }
+
+    double weigth_;
+    double height_;
+    double scale_;
+    double a_;
+};
 
 /// Global Variables
-const int alpha_slider_max =500;
+///
+//TODO : Extract UI to sepatate
+const int alpha_slider_max =10;
 int alpha_slider = 1;
 double alpha = 1;
 double beta = 0;
-///**
-// * @function on_trackbar
-// * @brief Callback for trackbar
-// */
-void on_a_changed( int val, void* user_data ) {
+
+
+typedef std::function<void(int val)> ChangeFunc;
+
+void on_changed( int val, void* user_data ) {
     assert(user_data);
-    FuncR* func = reinterpret_cast<FuncR*>(user_data);
-    func->a_ =  val;
-    func->Draw();
+    ChangeFunc* func = reinterpret_cast<ChangeFunc*>(user_data);
+    (*func)(val);
 }
-
-void on_b_changed( int val, void* user_data ) {
-    assert(user_data);
-    FuncR* func = reinterpret_cast<FuncR*>(user_data);
-    func->b_ =  val;
-    func->Draw();
-}
-
-void on_c_changed( int val, void* user_data ) {
-    assert(user_data);
-    FuncR* func = reinterpret_cast<FuncR*>(user_data);
-    func->c_ =  val;
-    func->Draw();
-}
-
-void on_d_changed( int val, void* user_data ) {
-    assert(user_data);
-    FuncR* func = reinterpret_cast<FuncR*>(user_data);
-    func->d_ =  val;
-    func->Draw();
-}
-
-void on_e_changed( int val, void* user_data ) {
-    assert(user_data);
-    FuncR* func = reinterpret_cast<FuncR*>(user_data);
-    func->e_ =  val;
-    func->Draw();
-}
-
-
-
-//void on_changed( int val, void* user_data ) {
-//   assert(user_data);
-//   FuncR* func = reinterpret_cast<FuncR*>(user_data);
-//   func->Draw();
-//}
 
 
 int main() {
-    FuncR function;
     cv::namedWindow(WINDOW_NAME);
 
-    cv::createTrackbar( "a ", WINDOW_NAME, 0, alpha_slider_max, on_a_changed
-                        ,&function);
-    cv::createTrackbar( "b ", WINDOW_NAME, 0, alpha_slider_max, on_b_changed,
-                        &function);
-    cv::createTrackbar( "c ", WINDOW_NAME, 0, alpha_slider_max, on_c_changed,
-                        &function);
-    cv::createTrackbar( "d ", WINDOW_NAME, 0, alpha_slider_max, on_d_changed,
-                        &function);
-    cv::createTrackbar( "e ", WINDOW_NAME, 0, alpha_slider_max, on_e_changed,
-                        &function);
-    on_a_changed(alpha_slider, &function);
+    House house(1,1,0.9,1);
+    ChangeFunc on_scale_change = [&house] (int val){
+        house.scale_ = (val+1)/10.0;
+        house.Draw();
+    };
 
+    ChangeFunc on_a_change = [&house] (int val){
+        house.a_ = (val)/10.0;
+        house.Draw();
+    };
 
-    function.Draw();
+    cv::createTrackbar( "a ", WINDOW_NAME, &alpha_slider, alpha_slider_max, on_changed,
+                        &on_a_change);
+//    cv::createTrackbar( "b ", WINDOW_NAME, &alpha_slider, alpha_slider_max, on_changed,
+//                        &on_b_change);
+//    cv::createTrackbar( "c ", WINDOW_NAME, &alpha_slider, alpha_slider_max, on_changed,
+//                        &on_c_change);
+
+    cv::createTrackbar( "scale ", WINDOW_NAME, &alpha_slider, alpha_slider_max, on_changed,
+                        &on_scale_change);
+    house.Draw();
     cv::waitKey(0);
-    //    while (true) {
-    //        double a;
 
-    //        cv::waitKey(0);
-    //    }
     return 0;
-    //    std::complex<double> end = 50. + 100i;
-    //    std::complex<double> step = 1 + 1i;
-    //    std::complex<double> a, b, c, d, e;
-    //    a = 5 + 3i;
-    //    b = 1 + 1i;
-    //    c = 2 + 2i;
-    //    d = 1 + 1i;
-    //    std::complex<double> z = 40. + 10i;
-    //    std::complex<double> result;
-    //    while((z.real() < end.real()) && z.imag() < end.imag()){
-    //        result = func(b, c, e, a, d, z);
-    //        z = z + step;
-    //        generate(result);
-    //    }
-    //    return 0;
 }
 
--- ComplexFunction/svg_graph.h
+++ ComplexFunction/svg_graph.h
@@ -6,8 +6,20 @@
 #include <fstream>
 #include <assert.h>
 #include <sstream>
+#include <map>
+#include <string>
 namespace svg {
 const std::string  XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>";
+
+enum SVGColor{
+    COLOR_RED,
+    COLOR_BLUE,
+    COLOR_GREEN
+};
+
+static std::map<SVGColor, std::string> m = {{COLOR_RED, "stroke:rgba(250, 0, 0, 255);"},
+                                               {COLOR_BLUE, "stroke:rgba(0, 0, 250, 255);"},
+                                               {COLOR_GREEN, "stroke:rgba(0, 250, 0, 255);"}};
 
 std::string create_svg_header(int w, int h ) {
     std::stringstream ss;
@@ -19,11 +31,13 @@ std::string create_svg_header(int w, int h ) {
     return ss.str();
 }
 
+static int radius = 1;
 class SVGGraph {
+
 public:
+
     SVGGraph(const std::string& file, int weight, int height)
         :file_stream_(file.c_str()),
-          style_("stroke:rgba(250, 0, 0, 255);stroke-width:2"),
         weight_(weight), height_(height) {
         assert(file_stream_.is_open());
         file_stream_ << XML_HEADER << create_svg_header(weight, height) << std::endl;
@@ -34,7 +48,7 @@ public:
         file_stream_.close();
     }
 
-    virtual void line(int x1, int y1, int x2, int y2) {
+    virtual void line(int x1, int y1, int x2, int y2, SVGColor color) {
 
         file_stream_ << std::endl;
         file_stream_ << "<line ";
@@ -42,30 +56,84 @@ public:
         file_stream_ << " y1= \"" << y1 <<"\"";
         file_stream_ << " x2= \"" << x2 <<"\"";
         file_stream_ << " y2= \"" << y2 <<"\"";
-        file_stream_ << " style = \"" << style_ << "\"/>";
+        file_stream_ << " style = \"" << m[color] << "stroke-width:2" << "\"/>";
         file_stream_ << std::endl;
 
     }
 
-    std::string style_;
+    virtual void point(int x, int y, std::string color_point){
+        file_stream_ << std::endl;
+        file_stream_ << "<circle ";
+        file_stream_ << " cx= \"" << x << "\"";
+        file_stream_ << " cy= \"" << y << "\"";
+        file_stream_ << " r = \"" << radius << "\"";
+        file_stream_ << " stroke= \"" << color_point << "\"";
+        file_stream_ << " stroke-width=\"3\" fill=\"none\" />";
+    }
+
     std::ofstream file_stream_;
     int weight_;
     int height_;
+
 };
 
-class SVGGraphMoved : public SVGGraph {
+static int kweight = 500;
+static int kheight = 500;
+
+class PlotSVG {
+private:
+    SVGGraph graph;
+    double x_max_;
+    double y_max_;
+
 public:
-    SVGGraphMoved(const std::string& file, int weight, int height)
-        :SVGGraph(file, weight, height){}
-
-    void line(int x1, int y1, int x2, int y2) override{
-        x1 += (weight_/2);
-        x2 += (weight_/2);
-        y1 += (height_/2);
-        y2 += (height_/2);
-        SVGGraph::line(x1, y1, x2, y2);
-
+    PlotSVG(const std::string& file, double x_max, double y_max)
+        :graph(file, kweight, kheight), x_max_(x_max), y_max_(y_max){
+        line(-x_max_, 0, x_max_, 0, COLOR_BLUE);
+        line(0, -y_max_, 0, y_max_, COLOR_BLUE);
+        point(0, 0, "black");
     }
+
+    void point (double x, double y, std::string color_point) {
+        // TODO : Add color with default parameter
+        x = x + x_max_;
+        x = x * (kweight / (x_max_ * 2 ));
+        y = y_max_ - y;
+        y = y * (kheight / (y_max_ * 2 ));
+        graph.point(x, y, color_point);
+    }
+
+    void line(double x1, double y1, double x2, double y2, SVGColor color) {
+        // TODO : Add color with default parameter
+        // TODO : Extract transform to pixel
+        // TODO : Extract move to positive functions
+
+        move_coordinates_x(x1, x2);
+        move_coordinates_y(y1, y2);
+        transform(x1, y1, x2, y2);
+
+        std::cout << x1 << " " << y1 << " " << x2 << " " << y2 << std::endl;
+        graph.line(x1, y1, x2, y2, color);
+    }
+
+    void move_coordinates_x(double& x1, double& x2){
+        x1 = x1 + x_max_;
+        x2 = x2 + x_max_;
+    }
+
+    void move_coordinates_y( double& y1, double& y2){
+        y1 = y_max_ - y1;
+        y2 = y_max_ - y2;
+    }
+
+    void transform(double& x1, double& y1, double& x2, double& y2){
+        x1 = x1 * (kweight / (x_max_ * 2 ));
+        x2 = x2 * (kweight / (x_max_ * 2 ));
+        y1 = y1 * (kheight / (y_max_ * 2 ));
+        y2 = y2 * (kheight / (y_max_ * 2 ));
+    }
+
+
 };
 
 }
